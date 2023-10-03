893e166d411d05e6178a706735bb824828eba51e
mohsinraz3 <mohsinraz3@gmail.com>
2023-10-04 01:48:43 +0500

    edge eror fix

--- src/components/opengraph-image.tsx
+++ src/components/opengraph-image.tsx
@@ -19,7 +19,7 @@ export default async function OpengraphImage(props?: Props): Promise<ImageRespon
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
           <LogoIcon width="64" height="58" fill="white" />
         </div>
-        <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
+        <p tw="mt-12 text-6xl font-black text-white">{title}</p>
       </div>
     ),
     {
@@ -32,7 +32,7 @@ export default async function OpengraphImage(props?: Props): Promise<ImageRespon
             res.arrayBuffer()
           ),
           style: 'normal',
-          weight: 700
+          weight: 100
         }
       ]
     }
